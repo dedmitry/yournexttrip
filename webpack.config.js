3adf9078cfe527a39f5be4eb402e880249829b33
Dmitry Derevyanko <dedmitriy@gmail.com>
2026-05-17 21:48:44 -0400

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = (env = {}) => {
   const isDev = env.development === true;
