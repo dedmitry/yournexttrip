7339b8410c095b46520d49b29036a8993a59bb20
Dmitry Derevyanko <dedmitriy@gmail.com>
2026-05-17 22:21:22 -0400

    make app better

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Main from "@pages/Main";
-//import NotFound from "@pages/404";
+import NotFound from "@pages/404";
 
 
 const App: React.FC = () => {
     return (
         <Routes>
             <Route path="/" element={<Main />} />
-            
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 };
 
 export default App;
-
-
-/*
-<Route path="*" element={<NotFound />} />
-*/
